54df39851a43b8085cdc81db3941ccfc9f84a45d
Apekshaugale <apekshaugale2020@gmail.com>
2026-07-23 18:39:37 +0530

    identity operator

---
+++ pyhton/day16/preactice.py
@@ -0,0 +1,26 @@
+Python 3.14.6 (tags/v3.14.6:c63aec6, Jun 10 2026, 10:26:10) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 2 & 4
+0
+>>> 
+>>> 2 | 4
+6
+>>> 2 ^ 4
+6
+>>> 
+>>> ~59
+-60
+>>> 
+>>> ~-59
+58
+>>> 17>>2
+4
+>>> 
+>>> 17<<2
+68
+>>> 
+>>> 
+>>> a=[5,56,87]
+>>> a.reverse()
+>>> a
+[87, 56, 5]
